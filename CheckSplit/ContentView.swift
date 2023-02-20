db1693494e51b575d6e0188ce288aa78cceb76d4
saysetel <saysetel@gmail.com>
2023-02-20 20:38:46 +0100

    add app icon

--- CheckSplit/ContentView.swift
+++ CheckSplit/ContentView.swift
@@ -8,14 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sum = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipProcent = 10
+    @FocusState private var isTyping: Bool
+    private let rangeOfTips = [0, 5, 10, 15, 20, 25]
+    private let backgroundColor: Color = Color(red: 126/255, green: 175/255, blue: 72/255)
+    private let currencyFotmatter: FloatingPointFormatStyle<Double>.Currency = .currency(code:Locale.current.identifier)
+    private var amountPerPerson:  Double  {
+        let people = Double(numberOfPeople + 2)
+        let chosenAmountOfTips = Double(tipProcent)
+        let totalTips = chosenAmountOfTips * (sum / 100)
+        let total = sum + totalTips
+        return total / people
+    }
+    private var total:  Double  {
+        let chosenAmountOfTips = Double(tipProcent)
+        let totalTips = chosenAmountOfTips * (sum / 100)
+        return sum + totalTips
+    }
+    
+    private var totalTips:  Double  {
+        let chosenAmountOfTips = Double(tipProcent)
+        return chosenAmountOfTips * (sum / 100)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $sum, format:
+                            .currency(code:Locale.current.identifier))
+                    .keyboardType(.decimalPad)
+                    .focused($isTyping)
+                        
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<30) {
+                            Text("\($0) people")
+                        }
+                    }
+                } header: {
+                    Text("Enter your data")
+                        .foregroundColor(.black)
+                        .bold()
+                }
+                Section {
+                    Picker("Tips", selection: $tipProcent) {
+                        ForEach(rangeOfTips, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }header: {
+                    Text("Choose amount of tips")
+                        .foregroundColor(.black)
+                        .bold()
+                }
+                
+                Section {
+                    Text("Per person:")
+                        .foregroundColor(.black)
+                        .bold()
+                    Text(amountPerPerson, format: currencyFotmatter)
+                    Text("Total:")
+                        .foregroundColor(.black)
+                        .bold()
+                    Text(total, format: currencyFotmatter)
+                    Text("Total amount of tips:")
+                        .foregroundColor(.black)
+                        .bold()
+                    Text(totalTips, format: currencyFotmatter)
+                }
+            }
+            .navigationTitle(Text("CheckSplit"))
+            .navigationBarTitleDisplayMode(.inline)
+            .scrollContentBackground(.hidden)
+            .background(backgroundColor)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isTyping = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
